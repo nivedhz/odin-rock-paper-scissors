9279a99b5ee12c2593d1b602cd0b5581b89a1a5f
Nivedh E R <nivedher12@gmail.com>
2026-02-07 04:08:31 +0000

    Added some comments to show what everything does

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-const gameOptions = ["rock", "paper", "scissors"];
+const gameOptions = ["rock", "paper", "scissors"]; //The Default choices of the game
+// Below are the values that the computer can beat the user.
 const computerBeats = {
         rock : "paper",
         paper : "scissors",
         scissors : "rock"
     }
 
+// Gets the User's choice from a prompt window
 function getUserChoice(){
     let userChoice = prompt("Rock, Paper or Scissors: ").toLowerCase().trim();
     return userChoice;
 }
 
+// Gets the computer's choice with the gameOptions list and Math.random()
 function getComputerChoice(){
     let randomChoice = Math.floor(Math.random()*gameOptions.length);
     let computerChoice = gameOptions[randomChoice];
     return computerChoice;
 }
 
+// Simulates a round which calls the getUserChoice and getComputerChoice function
+// Logs the choice of user and computer into the console
+// Validate the input first and then check if the input results are draw, win or lose
 function round(){
     let user = getUserChoice();
     let computer = getComputerChoice();
@@ -36,4 +42,5 @@ function round(){
     }
 }
 
+// TODO: Temporary log which prints the return value of the round function,
 console.log(round())
