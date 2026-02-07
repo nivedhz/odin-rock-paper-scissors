be3a45c0c5814ee54e421f004cd04968de9b01c6
Nivedh E R <nivedher12@gmail.com>
2026-02-07 04:07:45 +0000

    Declared a computer beats array globally and used that to check the state, implemented validation check first, changed the function named of userChoice to getUserChoice and computerChoice to getComputerChoice

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 const gameOptions = ["rock", "paper", "scissors"];
+const computerBeats = {
+        rock : "paper",
+        paper : "scissors",
+        scissors : "rock"
+    }
 
-function userChoice(){
+function getUserChoice(){
     let userChoice = prompt("Rock, Paper or Scissors: ").toLowerCase().trim();
     return userChoice;
 }
-function computerChoice(){
+
+function getComputerChoice(){
     let randomChoice = Math.floor(Math.random()*gameOptions.length);
     let computerChoice = gameOptions[randomChoice];
     return computerChoice;
 }
+
 function round(){
-    let user = userChoice();
-    let computer = computerChoice();
+    let user = getUserChoice();
+    let computer = getComputerChoice();
+
     console.log(`You Chose: ${user.toUpperCase()}`)
     console.log(`Computer Chose: ${computer.toUpperCase()}`)
-    if (user == computer){
-        return `So it's a draw 🤖🧑‍🦰`;
-    }
-    else if((user == "rock" && computer == "paper")||(user == "paper" && computer == "scissors")||(user == "scissors" && computer == "rock")){
-        return `So Computer Wins 🤖`;
-    }
-    else if((user == "rock" && computer == "scissors") || (user == "paper" && computer == "rock") || (user == "scissors" && computer == "paper")){
-        return `So You Win 🧑‍🦰`
-    }
-    else{
+    if (!gameOptions.includes(user)){
         return "Please enter a valid input 👽"
     }
+    else if (user === computer){
+        return `So it's a draw 🤖🧑‍🦰`;
+    }
+    else if(computerBeats[user] === computer){
+        return `So Computer Wins 🤖`;
+    }
+    else {
+        return `So You Win 🧑‍🦰`
+    }
 }
+
 console.log(round())
