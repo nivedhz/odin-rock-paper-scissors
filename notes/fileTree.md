1472bc669206fcabd15a2cbe1590dc3d3befe3e4
Nivedh E R <nivedher12@gmail.com>
2026-02-15 08:39:40 +0000

    add: added filetree, background color | fix: fixed opacity of container and images | modulise: modulised some repeatings as functions

---
+++ notes/fileTree.md
@@ -0,0 +1,19 @@
+# File Tree: odin-rock-paper-scissors
+
+```
+├── 📁 audio
+│   ├── 🎵 menu-selection.mp3
+│   └── 🎵 menu-switching.mp3
+├── 📁 images
+│   ├── 🖼️ paper.png
+│   ├── 🖼️ rock.png
+│   └── 🖼️ scissors.png
+├── 📁 notes
+│   └── 📝 mindPlan.md
+├── 📁 script
+│   ├── 📄 script.js
+│   └── 📄 test.js
+├── 📁 style
+│   └── 🎨 style.css
+└── 🌐 index.html
+```
--- script/test.js
+++ script/test.js
@@ -29,43 +29,47 @@ const menuSelectionAudio = new Audio("audio/menu-selection.mp3");
 menuSelectionAudio.preload = "auto";
 menuSelectionAudio.volume = 0.3;
 
-// D-pad Button Functions.
-let rightBtnClick = rightBtn.addEventListener("click", function(){
+function addSelectionStyle(){
+    gameOptions.forEach(gameOption => {
+        gameOption.classList.remove("selection");
+    })
+    gameOptions[currentIndex].classList.add("selection");
+}
+function playSwitchingAudio(){
     menuSwitchingAudio.currentTime = 0;
     menuSwitchingAudio.play();
+}
+function playSelectionAudio(){
+    menuSelectionAudio.currentTime = 0;
+    menuSelectionAudio.play();
+}
+// D-pad Button Functions.
+let rightBtnClick = rightBtn.addEventListener("click", function(){
+    playSwitchingAudio();
     currentIndex++
     if (currentIndex > (gameOptions.length-1)){
         currentIndex = 0;
     }    
-    gameOptions.forEach(gameOption => {
-        gameOption.classList.remove("selection");
-    })
-    gameOptions[currentIndex].classList.add("selection");
+    addSelectionStyle();
 })
 
 let leftBtnClick = leftBtn.addEventListener("click", function(){
-    menuSwitchingAudio.currentTime = 0;
-    menuSwitchingAudio.play();
+    playSwitchingAudio();
     currentIndex--;
     if (currentIndex < 0){
-        currentIndex = 2;
+        currentIndex = (gameOptions.length -1);
     }
-    gameOptions.forEach(gameOption => {
-        gameOption.classList.remove("selection");
-    })
-    gameOptions[currentIndex].classList.add("selection");
+    addSelectionStyle();
 })
 
 // A-B Selection button function.
 let selectionBtnClick = selectionBtn.addEventListener("click", function(){
-    menuSelectionAudio.currentTime = 0;
-    menuSelectionAudio.play();
+    playSelectionAudio();
     let computerChoiceRandom = Math.floor(Math.random()*gameOptions.length);
     computerSelection.src = gameOptions[computerChoiceRandom].getAttribute("src")
     userSelection.src = gameOptions[currentIndex].getAttribute("src");
     
 })
 let menuBtnClick = menuBtn.addEventListener("click", function(){
-    menuSelectionAudio.currentTime = 0;
-    menuSelectionAudio.play();
+    playSelectionAudio();
 })
