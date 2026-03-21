8e55b9e7911c0d964bddfe67da9a3a63898e9264
Nivedh E R <nivedher12@gmail.com>
2026-03-21 18:51:19 +0530

    refactor(codebase): changed the spacing of index from 4 to 2

--- script/script.js
+++ script/script.js
@@ -15,27 +15,26 @@ const innerScreenElements = [...innerScreen.children];
 //Default Game Options
 const gameOptions = ["rock", "paper", "scissors"];
 const gameOptionsDOM = {
-    paper : paperOption,
-    rock : rockOption,
-    scissors : scissorsOption
-}
+  paper: paperOption,
+  rock: rockOption,
+  scissors: scissorsOption,
+};
 const computerBeats = {
-    rock: "paper",
-    paper: "scissors",
-    scissors: "rock"
-}
+  rock: "paper",
+  paper: "scissors",
+  scissors: "rock",
+};
 const MAX_ROUNDS = 5;
-
 
 // Defualt Technical Options
 const keyPressFunctions = {
-    "ArrowRight": rightBtnFunction,
-    "ArrowLeft": leftBtnFunction,
-    "Enter": aBtnFunction,
-    "r": bBtnFunction,
-    "R": bBtnFunction
-}
-const DEFAULT_SELCTION_IMAGE = "images/rock.png"
+  ArrowRight: rightBtnFunction,
+  ArrowLeft: leftBtnFunction,
+  Enter: aBtnFunction,
+  r: bBtnFunction,
+  R: bBtnFunction,
+};
+const DEFAULT_SELCTION_IMAGE = "images/rock.png";
 let rounds = 0;
 let currentUserScore = 0;
 let currentComputerScore = 0;
@@ -48,112 +47,112 @@ gameOptionValue[currentIndex].classList.add("selection");
 
 // Audios
 let audios = {
-    "menuSwitching" : new Audio("audio/menu-switching.mp3"),
-    "menuSelection" : new Audio("audio/menu-selection.mp3"),
-    "loseAudio" : new Audio("audio/lose-effect.mp3"),
-    "winAudio" : new Audio("audio/win-effect.mp3"),
-    "drawAudio" : new Audio("audio/draw-effect.mp3")
-}
-for (let [_, value] of Object.entries(audios)){
-    value.preload = "auto";
-    value.volume = 0.15;
-}
-
-function addSelectionStyle(){
-    gameOptionValue.forEach(gameOption => {
-        gameOption.classList.remove("selection");
-    })
-    gameOptionValue[currentIndex].classList.add("selection");
-}
-
-function playAudio(audio){
-    audio.currentTime = 0;
-    audio.play();
-}
-
-function addDefaultImage(){
-    computerSelection.src = userSelection.src = DEFAULT_SELCTION_IMAGE;
-}
-
-function endMessage(){
-    innerScreen.replaceChildren();
-    let message = document.createElement('div');
-    if(currentUserScore === currentComputerScore){
-        playAudio(audios.drawAudio);
-        message.textContent = "It's a Draw!";
-    }
-    else if(currentUserScore < currentComputerScore){
-        playAudio(audios.loseAudio);
-        message.textContent = "Computer Wins!";
-    }
-    else{
-        playAudio(audios.winAudio);
-        message.textContent = "You Win!";
-    }
-    message.classList.add("message");
-    innerScreen.appendChild(message);
-    rounds = currentUserScore = currentComputerScore = 0;
-}
-
-function playRound(userSelectionKey, computerSelectionKey){
-    if (userSelectionKey === computerSelectionKey){
-        currentUserScore+= 0.5;
-        currentComputerScore+= 0.5;
-    }
-    else if(computerBeats[userSelectionKey] === computerSelectionKey){
-        currentComputerScore+= 1;
-    }
-    else{
-        currentUserScore+= 1;
-    }
-    userScore.textContent = currentUserScore;
-    computerScore.textContent = currentComputerScore;
-}
-
-function rightBtnFunction(){
-    playAudio(audios.menuSwitching);
-    currentIndex++;
-    if (currentIndex > (gameOptionKey.length-1)){
-        currentIndex = 0;
-    }
-    addSelectionStyle();
-}
-
-function leftBtnFunction(){
-    playAudio(audios.menuSwitching);
-    currentIndex--;
-    if (currentIndex < 0){
-        currentIndex = (gameOptionKey.length-1);
-    }
-    addSelectionStyle();
-}
-
-function aBtnFunction(){
-    playAudio(audios.menuSelection);
-    let computerChoiceRandom = Math.floor(Math.random()*gameOptionKey.length);
-    let userSelectionKey = gameOptionKey[currentIndex];
-    let computerSelectionKey = gameOptionKey[computerChoiceRandom];
-    let computerSelectionClass = gameOptionValue[computerChoiceRandom];
-    let userSelectionClass = gameOptionValue[currentIndex];
-    computerSelection.src = computerSelectionClass.getAttribute("src");
-    userSelection.src = userSelectionClass.getAttribute("src");
-    rounds++;
-    if (rounds>MAX_ROUNDS){
-        endMessage();
-    }
-    else {
-        playRound(userSelectionKey, computerSelectionKey);
-    }
+  menuSwitching: new Audio("audio/menu-switching.mp3"),
+  menuSelection: new Audio("audio/menu-selection.mp3"),
+  loseAudio: new Audio("audio/lose-effect.mp3"),
+  winAudio: new Audio("audio/win-effect.mp3"),
+  drawAudio: new Audio("audio/draw-effect.mp3"),
 };
+for (let [_, value] of Object.entries(audios)) {
+  value.preload = "auto";
+  value.volume = 0.15;
+}
 
-function bBtnFunction(){
-    playAudio(audios.menuSelection);
-    innerScreen.replaceChildren(...innerScreenElements);
-    rounds = currentUserScore = currentComputerScore = userScore.textContent = computerScore.textContent = 0;
-    currentIndex = 1;
-    addDefaultImage();
-    addSelectionStyle();
-};
+function addSelectionStyle() {
+  gameOptionValue.forEach((gameOption) => {
+    gameOption.classList.remove("selection");
+  });
+  gameOptionValue[currentIndex].classList.add("selection");
+}
+
+function playAudio(audio) {
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function addDefaultImage() {
+  computerSelection.src = userSelection.src = DEFAULT_SELCTION_IMAGE;
+}
+
+function endMessage() {
+  innerScreen.replaceChildren();
+  let message = document.createElement("div");
+  if (currentUserScore === currentComputerScore) {
+    playAudio(audios.drawAudio);
+    message.textContent = "It's a Draw!";
+  } else if (currentUserScore < currentComputerScore) {
+    playAudio(audios.loseAudio);
+    message.textContent = "Computer Wins!";
+  } else {
+    playAudio(audios.winAudio);
+    message.textContent = "You Win!";
+  }
+  message.classList.add("message");
+  innerScreen.appendChild(message);
+  rounds = currentUserScore = currentComputerScore = 0;
+}
+
+function playRound(userSelectionKey, computerSelectionKey) {
+  if (userSelectionKey === computerSelectionKey) {
+    currentUserScore += 0.5;
+    currentComputerScore += 0.5;
+  } else if (computerBeats[userSelectionKey] === computerSelectionKey) {
+    currentComputerScore += 1;
+  } else {
+    currentUserScore += 1;
+  }
+  userScore.textContent = currentUserScore;
+  computerScore.textContent = currentComputerScore;
+}
+
+function rightBtnFunction() {
+  playAudio(audios.menuSwitching);
+  currentIndex++;
+  if (currentIndex > gameOptionKey.length - 1) {
+    currentIndex = 0;
+  }
+  addSelectionStyle();
+}
+
+function leftBtnFunction() {
+  playAudio(audios.menuSwitching);
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = gameOptionKey.length - 1;
+  }
+  addSelectionStyle();
+}
+
+function aBtnFunction() {
+  playAudio(audios.menuSelection);
+  let computerChoiceRandom = Math.floor(Math.random() * gameOptionKey.length);
+  let userSelectionKey = gameOptionKey[currentIndex];
+  let computerSelectionKey = gameOptionKey[computerChoiceRandom];
+  let computerSelectionClass = gameOptionValue[computerChoiceRandom];
+  let userSelectionClass = gameOptionValue[currentIndex];
+  computerSelection.src = computerSelectionClass.getAttribute("src");
+  userSelection.src = userSelectionClass.getAttribute("src");
+  rounds++;
+  if (rounds > MAX_ROUNDS) {
+    endMessage();
+  } else {
+    playRound(userSelectionKey, computerSelectionKey);
+  }
+}
+
+function bBtnFunction() {
+  playAudio(audios.menuSelection);
+  innerScreen.replaceChildren(...innerScreenElements);
+  rounds =
+    currentUserScore =
+    currentComputerScore =
+    userScore.textContent =
+    computerScore.textContent =
+      0;
+  currentIndex = 1;
+  addDefaultImage();
+  addSelectionStyle();
+}
 
 // Default Functions
 addDefaultImage();
@@ -162,14 +161,11 @@ addDefaultImage();
 rightBtn.addEventListener("click", rightBtnFunction);
 leftBtn.addEventListener("click", leftBtnFunction);
 
-
-document.addEventListener("keydown", function(event) {
-    if (keyPressFunctions[event.key]){
-        keyPressFunctions[event.key]();
-    }
+document.addEventListener("keydown", function (event) {
+  if (keyPressFunctions[event.key]) {
+    keyPressFunctions[event.key]();
+  }
 });
-
-
 
 // A-B Selection button function.
 selectionBtn.addEventListener("click", aBtnFunction);
